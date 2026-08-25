686b8e16164aeeda710d710f6d7612bc7cb878a0
samuraishadow877-droid <samuraishadow877@gmail.com>
2026-08-25 20:42:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -355,21 +355,14 @@ function initializeMap() {
     /* OpenStreetMap tiles */
 
     L.tileLayer(
-
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-
-        {
-
-            maxZoom: 19,
-
-            attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
-
-        }
-
-    ).addTo(
-        lifelinkMap
-    );
+    "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+    {
+        maxZoom: 20,
+        attribution:
+            '&copy; OpenStreetMap contributors &copy; CARTO',
+        subdomains: "abcd"
+    }
+).addTo(lifelinkMap);
 
 
     renderMapMarkers();
